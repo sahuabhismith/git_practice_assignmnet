ae71ed41bc5b956a479c4590ee7840e4c1d16997
sahuabhismith <sahuabhismith@gmail.com>
2022-10-19 15:58:24 +0530

    A number is prime or not and A string is palindrome or not

---
+++ palindrome.js
@@ -0,0 +1,6 @@
+let str = "madam";
+let flag = "Yes";
+for(i=0;i<=str.length-1;i++){
+flag = "No";
+}
+console.log(flag);
---
+++ prime.js
@@ -0,0 +1,11 @@
+let count =0;
+for(i=1;i<=num;i++){
+if(num%i == 0){
+count++;
+}
+}
+if (count ==  2){
+console.log("No");
+}else{ 
+console.log("Yes");
+}
